087c1f936b65969f6c3e6101218b2dd7d3d247de
zhujx <18826230882@163.com>
2022-08-03 22:14:26 +0800

    增加技能树

--- game.py
+++ game.py
@@ -4,17 +4,20 @@ import os
 import random
 import sys
 import copy
+from skill_tree import Skill_Tree
+from skill_tree import Skill_Value
 
 #人物基本面板  攻击力 防御力 生命值 治疗值 蓝条  药水 debuff buff 
 #攻击力=基本攻击力+武器符文加成攻击力+符石加成
 #人物A B C D E
 class Huaxing(object):
     #攻击力 防御力 生命值
-     def __init__(self,Name,Damage,Defense,Life_Value) :
+     def __init__(self,Name,Damage,Defense,Life_Value,Skill_Name) :
         self.Name=Name   #名称
         self.Damage=Damage  #攻击力
         self.Defense=Defense #防御力
         self.Life_Value=Life_Value #生命值
+        self.Skill_Name=Skill_Name
     
      def Run_Skill(self,x) :
         print("running Skill 造成伤害",x)
@@ -71,7 +74,11 @@ def HX_Round(HX,boss):
     Current_Buf(HX,boss)
     Current_Debuf(HX,boss)
     
-    HX[0].Run_Skill(boss.Defense)
+    #HX[0].Run_Skill(boss.Defense)
+    for x in HX :
+        print('使用技能:'+x.Skill_Name,Skill_Tree.get(x.Skill_Name))
+
+    print('造成实际伤害',x.Damage * Skill_Value(x.Skill_Name) -boss.Defense)
 
 def Boss_Round(HX,boss):
     #当前buf和debuf
@@ -102,15 +109,16 @@ def main():
     #人物基本面板  攻击力 防御力 生命值 治疗值 蓝条  药水 debuff buff 
     #没人上限6格魔晶 初始6格 每回合恢复2格魔晶
 
-    Huaxing_A = Huaxing('Huaxing_A',99,100,999)
-    Huaxing_B = Huaxing('Huaxing_B',98,101,998)
-    Huaxing_C = Huaxing('Huaxing_C',97,102,997)
-    Huaxing_D = Huaxing('Huaxing_D',96,103,996)
-    Huaxing_E = Huaxing('Huaxing_E',95,104,995)
+    #print(Skill_Tree.get('异色神光'))
+    Huaxing_A = Huaxing('Huaxing_A',99,100,999,'异色神光')
+    Huaxing_B = Huaxing('Huaxing_B',98,101,998,'大荒八卦阵')
+    Huaxing_C = Huaxing('Huaxing_C',97,102,997,'聚能投射')
+    Huaxing_D = Huaxing('Huaxing_D',96,103,996,'玄阳剑诀')
+    Huaxing_E = Huaxing('Huaxing_E',95,104,995,'灭魂针')
 
     HX=[Huaxing_A,Huaxing_B,Huaxing_C,Huaxing_D,Huaxing_E]
 
-    boss = Boss('Boss',1000,998,999)
+    boss = Boss('Boss',1000,208,999)
     #boss.Run_Skill('Boss')
 
     #将所有打印日志输出到一个文件中
---
+++ skill_tree.py
@@ -0,0 +1,26 @@
+  #技能树
+
+from nis import match
+import re
+from tkinter.messagebox import RETRY
+
+
+Skill_Tree = {'异色神光':'镇压敌方单体，造成攻击152%的伤害',
+            '大荒八卦阵':'绞杀敌方单体，造成攻击142%的伤害',
+            '聚能投射':'光线射击敌方单体，造成攻击148%的伤害',
+            '玄阳剑诀':'玄阳飞剑射向敌方单体，造成攻击138%的伤害',
+            '灭魂针':'灭魂针射向敌方单体，造成攻击168%的伤害',
+    }
+
+def Skill_Value(skill_name):
+    if skill_name == '异色神光':
+        return 1.52
+    elif skill_name == '大荒八卦阵':
+        return 1.42
+    elif skill_name == '聚能投射':
+        return 1.48
+    elif skill_name == '玄阳剑诀':
+        return 1.38
+    elif skill_name == '灭魂针':
+        return 1.68
+
